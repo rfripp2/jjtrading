a5f6ad801f2734fac759d19ae8ef948107448261
juan gonzalez venzano <juangonzalez465@gmail.com>
2022-02-23 11:55:58 -0300

    filter coins component

---
+++ src/Deploy/Components/CoinsExluded.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function CoinsExcluded({ props }) {
+  useEffect(() => {
+    axios
+      .get("https://jjtradingapi.herokuapp.com/minsmax/coinsexcluded")
+      .then((result) => setCoinsExcluded(result.data));
+  }, []);
+
+  const [coin, setCoin] = useState();
+  const [coinsExcluded, setCoinsExcluded] = useState([]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.put(
+      `https://jjtradingapi.herokuapp.com/minsmax/addcoin?coin=${coin}`
+    );
+    setCoinsExcluded([...coinsExcluded, coin]);
+  };
+  const handleOnChange = (event) => {
+    setCoin(event.target.value);
+    console.log(coin);
+  };
+  return (
+    <div>
+      <div className="coinsExcluded">
+        <h2>Coins exluded:</h2>
+        {coinsExcluded ? coinsExcluded.map((coin) => coin + " ") : ""}
+      </div>
+      <div className="addCoin">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Add coin to exclude:
+            <input
+              type="text"
+              value={coin}
+              name="coin"
+              onChange={handleOnChange}
+            ></input>
+          </label>
+
+          <input type="submit"></input>
+        </form>
+      </div>
+      {console.log("coins exluded:", coinsExcluded)}
+    </div>
+  );
+}
--- src/Deploy/Components/MinsMax.js
+++ src/Deploy/Components/MinsMax.js
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
+import styles from "./MinsMax.css";
+import CoinsExcluded from "./CoinsExluded";
 export const MinsMax = () => {
   const [state, setState] = useState({});
   const [result, setResult] = useState();
+  //const [coinsExcluded, setCoinsExcluded] = useState([]);
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
       .get(
-        `https://jjtradingapi.herokuapp.com/prices?days_back=${state.daysBack}&coins_quantity=${state.coinsQuantity}`
+        `https://jjtradingapi.herokuapp.com/minsmax?days_back=${state.daysBack}&coins_quantity=${state.coinsQuantity}`
       )
       .then((result) => {
         console.log(result.data);
@@ -29,6 +32,7 @@ export const MinsMax = () => {
   }
   return (
     <div>
+      <CoinsExcluded></CoinsExcluded>
       <form onSubmit={handleSubmit}>
         <label>
           Days Back:
@@ -51,15 +55,13 @@ export const MinsMax = () => {
         </label>
         <input type="submit"></input>
       </form>
-      {console.log(state)}
-      {console.log("result", result)}
-      <div class="container">
-        <div>
+      <div className="container">
+        <div className="flexInnerDiv">
           <h2>Max :</h2>
           {result ? <h3>Total: {result.max.total}</h3> : ""}
           {result ? result.max.coins.map((coin) => <p>{coin}</p>) : ""}
         </div>
-        <div>
+        <div className="flexInnerDiv">
           <h2>Min :</h2>
           {result ? <h3>Total: {result.min.total}</h3> : ""}
           {result ? result.min.coins.map((coin) => <p>{coin}</p>) : ""}
